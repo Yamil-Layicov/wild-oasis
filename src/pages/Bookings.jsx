a700f64030d39af4e0228e8992697155ea1765ab
Yamil <yamilbl@code.edu.az>
2024-01-28 19:18:38 +0400

    first commit

---
+++ src/pages/Bookings.jsx
@@ -0,0 +1,16 @@
+// import Heading from "../ui/Heading";
+// import Row from "../ui/Row";
+
+function Bookings() {
+  return (
+    // <Row type="horizontal">
+    //   <Heading as="h1">All bookings</Heading>
+    //   <p>TEST</p>
+    // </Row>
+    <>
+      <h1>Bookinig</h1>
+    </>
+  );
+}
+
+export default Bookings;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+// import Heading from "../ui/Heading";
+// import Row from "../ui/Row";
+
+function Dashboard() {
+  return (
+    // <Row type="horizontal">
+    //   <Heading as="h1">Dashboard</Heading>
+    //   <p>TEST</p>
+    // </Row>
+    <h1>Dashboard</h1>
+  );
+}
+
+export default Dashboard;
