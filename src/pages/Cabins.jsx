1bf19276fa4105016fb0a276dda1e7a08764e940
Yamil <yamilbl@code.edu.az>
2024-01-29 09:19:01 +0400

    addded reactQuery

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,20 @@
-// import Heading from "../ui/Heading";
-// import Row from "../ui/Row";
+import { useEffect } from "react";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
-function Cabins() {
+function Cabins() { 
+
+  useEffect(function(){
+    getCabins().then(data => console.log(data))
+  }, [])
+
   return (
-    // <Row type="horizontal">
-    //   <Heading as="h1">All cabins</Heading>
-    //   <p>TEST</p>
-    // </Row>
-    <h1>Cabins</h1>
+    <Row type="horizontal">
+      <Heading as="h1">All cabins</Heading>
+      <p>TEST</p>
+      <img src="https://vceibjkgjexkwcatxhzh.supabase.co/storage/v1/object/public/cabin-images/cabin-005.jpg" alt="" />
+    </Row>
   );
 }
 
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://vceibjkgjexkwcatxhzh.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjZWliamtnamV4a3djYXR4aHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY0NjAxNTYsImV4cCI6MjAyMjAzNjE1Nn0.UAbhKcPsSBwWpclKF5KxY4b_uLBDi8wnIFem4Cj5UAo";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
